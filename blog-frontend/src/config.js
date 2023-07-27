1a6270b9ae6a0cdafa0c6aef1e4f5397ff96a44b
Alex-C <alexcook1613@gmail.com>
2023-07-27 16:38:30 -0400

    updated config.js

--- blog-frontend/src/config.js
+++ blog-frontend/src/config.js
@@ -1,4 +1,4 @@
 // const API_BASE_URL = 'https://your-backend-app.herokuapp.com'; // Replace with the correct backend URL on Heroku
-const API_BASE_URL = 'https://museblog-2c85ee3e365c.herokuapp.com';
+const API_BASE_URL = 'https://museblog-2c85ee3e365c.herokuapp.com/api/blogs';
 
 export default API_BASE_URL;
